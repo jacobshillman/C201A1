ad06bbbc5fe2a2d61aa6d5a543346e4e2039764e
Jacob S Hillman <jacob.s.hillman@gmail.com>
2015-11-02 20:19:49 -0800

    Edits+reformats+JS&interests

---
+++ meJS.js
@@ -0,0 +1,18 @@
+//Swapping images:
+function bigJake() {
+  document.getElementById("thumb").src="Images/Jake-VA-small.jpg";
+}
+
+function littleJake() {
+  document.getElementById("thumb").src="Images/Jake-VA-tiny.jpg";
+}
+
+function bigBobby() {
+  document.getElementById("clown").src="Images/Bobby-DUI-small.jpg";
+}
+
+function littleBobby() {
+  document.getElementById("clown").src="Images/Bobby-DUI-tiny.jpg";
+}
+
+//

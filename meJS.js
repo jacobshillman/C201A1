0478371b3dc8a02ef27dd5cb2a15c1374979a022
Jacob S Hillman <jacob.s.hillman@gmail.com>
2015-11-07 16:26:43 -0800

    Merged Interests into gg

---
+++ meJS.js
@@ -0,0 +1,27 @@
+//Swapping images:
+function bigJake() {
+  document.getElementById("thumb").src="Images/Jake-VA-small.jpg";
+}
+
+function littleJake() {
+  document.getElementById("thumb").src="Images/Jake-VA-tiny.jpg";
+}
+
+function bigBobby() {
+  document.getElementById("clown").src="Images/Bobby-DUI-small.jpg";
+}
+
+function littleBobby() {
+  document.getElementById("clown").src="Images/Bobby-DUI-tiny.jpg";
+}
+
+//Contact Me:
+function agree() {
+  alert("I agree to the terms and conditions -especially donating my kidney.");
+}
+
+function today() {
+  var dtg = new Date();
+  document.getElementById("DTG").insertAdjacentHTML('beforeend', dtg);
+}
+
